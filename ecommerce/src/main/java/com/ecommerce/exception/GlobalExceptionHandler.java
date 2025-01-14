2bfa8a67b9755e9cb3b44218d7506ce6469f2d05
Mahendar Kumawat <mahendar710@gmail.com>
2025-01-14 21:28:58 +0530

    Global Exception Handler added.

---
+++ ecommerce/src/main/java/com/ecommerce/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.ecommerce.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        logger.info("Resource not found :{}", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+}
