c781606349619399ebc7bdadfe847ef9259ccaa5
Mahendar Kumawat <mahendar710@gmail.com>
2025-01-19 12:03:52 +0530

    unused imports

--- ecommerce/src/main/java/com/ecommerce/controller/ProductRestController.java
+++ ecommerce/src/main/java/com/ecommerce/controller/ProductRestController.java
@@ -1,11 +1,9 @@
 package com.ecommerce.controller;
 
 import com.ecommerce.ApiUrls;
-import com.ecommerce.entity.AppUser;
 import com.ecommerce.entity.Product;
 import com.ecommerce.service.ProductService;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
